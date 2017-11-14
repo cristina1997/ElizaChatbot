0838b55d58958551c676be01e655f4d554deeb3a
cristina1997 <cristinaflorinaeu@gmail.com>
2017-11-14 15:19:57 +0000

    Output form input

--- eliza.go
+++ eliza.go
@@ -1,7 +1,10 @@
 package main
 //Code: https://getbootstrap.com/docs/4.0/components/forms/
+//Take from form: https://stackoverflow.com/questions/23282311/parse-input-from-html-form-in-golang
+			// https://www.google.ie/search?q=parse+input+in+go+html&rlz=1C1GGRV_enIE769IE769&oq=parse+input+in+go+html&aqs=chrome..69i57.6111j0j4&sourceid=chrome&ie=UTF-8
 import (
 	"net/http"
+	"fmt"
 	//"text/template"
 	//"math/rand"
 	//"time"
@@ -17,13 +20,18 @@ type tempData struct {
 	Eliza string
 }
 
-func indexHTML(w http.ResponseWriter, r * http.Request){
-	http.ServeFile(w, r, "src/index.html")	
+func chatHandler(w http.ResponseWriter, r * http.Request) {
+	err := r.ParseForm()
+	checkError(err)
+
+	chat := r.FormValue("input")
+	fmt.Println(chat)
+	fmt.Fprintf(w, "Hello %s ", chat)	 
 }
 
 func main() {
-	http.HandleFunc("/", indexHTML)
-	//http.HandleFunc("/chat/", chatHandler)
+	http.Handle("/", http.FileServer(http.Dir("./src")))
+	http.HandleFunc("/chat/", chatHandler)
 
 	http.ListenAndServe(":9999", nil)
 }
