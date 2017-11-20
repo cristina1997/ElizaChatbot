fcb6c6d6f7eca903aa6b46d9797fcad4970a7b4f
Christina Nita <Christina Nita>
2017-11-20 22:54:01 +0000

    Changed js around

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,30 +1,29 @@
+// Creates constants for #list and #form id attributes from index
+const list = $("#list");
+const form = $("#form");
+
 form.keypress(function(event){
-    // Creates constants for #list and #form id attributes from index
-    const list = $("#list");
-    const form = $("#form");
 
     if(event.keyCode = 13){
          // form default refreshes the page and clears everything so we prevent it to make requests with JS
         event.preventDefault();
 
-        const inputText = form.val(); // store the value taken from the form input into inputText
+        const text = form.val(); // store the value taken from the form input into text
         form.val(""); // delete the value from the actual form input
 
         // if text is valid execute the following
-        if(text.trim()){
-            list.append("<li>" + text + "</li>");
-
-            $.get("/chat/", {input: inputText})
-            .done(function(w){ // "w" is the response from server
-                list.append("<li>" + w + "</li>");
-            }).fail(function(){ // it runs in case of an error (i.e. if the server isn't running)
-                list.append("<li>Sorry! Eliza is not here. Please come back later!</li>");
-            });
-
-           
-        } else if (!text.trim()){
+        if (!text.trim()){
             return
         }
+        
+        list.append("<li>" + text + "</li>");
+
+        $.get("/chat", {input: text})
+        .done(function(w){ // "w" is the response from server
+            list.append("<li>" + w + "</li>");
+        }).fail(function(){ // it runs in case of an error (i.e. if the server isn't running)
+            list.append("<li>Sorry! Eliza is not here. Please come back later!</li>");
+        });
     }
     else if (event.keyCode != 13){
         return
